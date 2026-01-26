09a97805c3473868ffd74a5cf9d914b2011f8db0
Max Leonhardt <m.leonhardt424@gmail.com>
2026-01-26 22:39:29 +0000

    add home manager

--- flake.nix
+++ flake.nix
@@ -1,12 +1,22 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+
+    home-manager.url = "github:nix-community/home-manager/release-25.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
-  outputs = inputs@{ self, nixpkgs, nixos-hardware, ... }: {
+  outputs = inputs@{ self, nixpkgs, home-manager, nixos-hardware, ... }: {
     nixosConfigurations.nixos-laptop = nixpkgs.lib.nixosSystem {
       modules = [
         ./hosts/nixos-laptop
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.max = ./hosts/nixos-laptop/home.nix;
+        }
         nixos-hardware.nixosModules.framework-amd-ai-300-series
       ];
     };
---
+++ hosts/nixos-laptop/home.nix
@@ -0,0 +1,8 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "max";
+  home.homeDirectory = "/home/max";
+
+  home.stateVersion = "25.11";
+}
