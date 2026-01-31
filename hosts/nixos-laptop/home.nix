ed09c2bc4a2c59e9c39d57106c26d314107b2114
Max Leonhardt <m.leonhardt424@gmail.com>
2026-01-31 21:34:02 +0000

    gtk dark theme

--- hosts/nixos-laptop/home.nix
+++ hosts/nixos-laptop/home.nix
@@ -12,5 +12,22 @@
   home.username = "max";
   home.homeDirectory = "/home/max";
 
+  dconf = {
+    enable = true;
+    settings = {
+      "org/gnome/desktop/interface" = {
+        color-scheme = "prefer-dark";
+      };
+    };
+  };
+
+  gtk = {
+    enable = true;
+    theme = {
+      name = "Breeze-Dark";
+      package = pkgs.kdePackages.breeze-gtk;
+    };
+  };
+
   home.stateVersion = "25.11";
 }
