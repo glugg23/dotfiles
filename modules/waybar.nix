d5dc6c72f3e015d0e55fb10fb438476aa731f8a4
Max Leonhardt <m.leonhardt424@gmail.com>
2026-01-31 23:12:43 +0000

    waybar basic niri integration

--- modules/waybar.nix
+++ modules/waybar.nix
@@ -4,12 +4,15 @@
   programs.waybar = {
     enable = true;
     settings.main = {
+      modules-left = [ "niri/workspaces" "niri/window" ];
       modules-right = [ "battery" "clock" ];
 
       "clock" = {
         format = "{:%d/%m/%Y %r}";
         interval = 1;
       };
+
+      "niri/window"."format" = "{app_id}";
     };
     style = ../config/waybar/style.css;
   };
