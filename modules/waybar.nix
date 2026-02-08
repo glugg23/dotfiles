01ce5cfc841ff003906e221d09c5296ee060a530
Max Leonhardt <m.leonhardt424@gmail.com>
2026-02-08 18:33:06 +0000

    cpu/memory styling

--- modules/waybar.nix
+++ modules/waybar.nix
@@ -6,16 +6,42 @@
     settings.main = {
       modules-left = [ "niri/workspaces" "niri/window" ];
       modules-center = [ "clock" ];
-      modules-right = [ "battery" "network" ];
+      modules-right = [ "cpu" "memory" "battery" "network" ];
+
+      "niri/workspaces" = {
+        format = "{icon}";
+        format-icons = {
+          main = "󰹑";
+          discord = "";
+        };
+      };
+
+      "niri/window".format = "{app_id}";
 
       clock = {
         format = "{:%d/%m/%Y %r}";
         interval = 1;
       };
 
+      cpu = {
+        states = {
+          warning = 50;
+          critical = 90;
+        };
+        format = "{usage}% ";
+      };
+
+      memory = {
+        states = {
+          warning = 75;
+          critical = 90;
+        };
+        format = "{percentage}% ";
+        tooltip-format = "{used:0.1f}/{total:0.1f} GiB";
+      };
+
       battery = {
         states = {
-          good = 80;
           warning = 50;
           critical = 20;
         };
@@ -32,17 +58,8 @@
         format-disconnected = "󰖪";
         tooltip-format = "{essid}";
       };
-
-      "niri/workspaces" = {
-        format = "{icon}";
-        format-icons = {
-          main = "󰹑";
-          discord = "";
-        };
-      };
-
-      "niri/window".format = "{app_id}";
     };
+
     style = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/config/waybar/style.css";
   };
 }
