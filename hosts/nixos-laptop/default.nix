5c18410d68d703444b08e844bea792100b3b6b51
Max Leonhardt <m.leonhardt424@gmail.com>
2026-02-08 18:48:41 +0000

    add spotify

--- hosts/nixos-laptop/default.nix
+++ hosts/nixos-laptop/default.nix
@@ -75,6 +75,7 @@
     git
     playerctl
     tree
+    spotify
     swaybg
     swaynotificationcenter
     vesktop
@@ -103,6 +104,7 @@
   # system.copySystemConfiguration = true;
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  nixpkgs.config.allowUnfree = true;
 
   # This option defines the first version of NixOS you have installed on this particular machine,
   # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
