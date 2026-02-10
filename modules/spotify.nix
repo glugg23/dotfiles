9906bbb964dbbddf7e2644066e8f5544dfa67a2f
Max Leonhardt <m.leonhardt424@gmail.com>
2026-02-10 21:04:58 +0000

    update spotify version

--- modules/spotify.nix
+++ modules/spotify.nix
@@ -7,12 +7,12 @@ let
   ];
   spotify = pkgs.spotify.overrideAttrs (
     finalAttrs: previousAttrs: {
-      version = "1.2.79.427.g80eb4a07";
-      rev = "91";
+      version = "1.2.82.428.g0ac8be2b";
+      rev = "92";
       src = pkgs.fetchurl {
         name = "spotify-${finalAttrs.version}-${finalAttrs.rev}.snap";
         url = "https://api.snapcraft.io/api/v1/snaps/download/pOBIoZ2LrCB3rDohMxoYGnbN14EHOgD7_${finalAttrs.rev}.snap";
-        hash = "sha512-wPg5J7xZUanyWQLO34IG/feXzI/4ljBh8bShqNgZJnG3yMb65v0Qxy7LoaoPqpmItEvlANhoA0esGCTVn4/5kA==";
+        hash = "sha512-/9lB4gLotYvM2QkHt8cKS8P4IXrBVzgoXEk4bWR3GQum0OnJqK/qCC9evmCZ7PAqbbyh5/8vSblM+QXXXiQiMA==";
       };
       env = previousAttrs.env // {
         librarypath = "${pkgs.lib.makeLibraryPath deps}:${previousAttrs.env.librarypath}";
