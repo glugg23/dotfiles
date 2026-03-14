05d13b2cef3fbc38fe2d79732e2420a1b3ba145a
Max Leonhardt <m.leonhardt424@gmail.com>
2026-03-14 18:39:14 +0000

    add more firefox extensions

--- modules/firefox.nix
+++ modules/firefox.nix
@@ -28,16 +28,27 @@
 
       ExtensionSettings =
         let
-          moz = short: "https://addons.mozilla.org/firefox/downloads/latest/${short}/latest.xpi";
-        in
-        {
-          "*".installation_mode = "blocked";
-
-          "uBlock0@raymondhill.net" = {
-            install_url = moz "ublock-origin";
+          default = {
             installation_mode = "force_installed";
             updates_disabled = true;
           };
+          private = default // {
+            private_browsing = true;
+          };
+          installExtention =
+            name: config:
+            {
+              install_url = "https://addons.mozilla.org/firefox/downloads/latest/${name}/latest.xpi";
+            }
+            // config;
+        in
+        {
+          "*".installation_mode = "blocked";
+          "uBlock0@raymondhill.net" = installExtention "ublock-origin" private;
+          "{446900e4-71c2-419f-a6a7-df9c091e268b}" = installExtention "bitwarden-password-manager" default;
+          "jid1-xUfzOsOFlzSOXg@jetpack" = installExtention "reddit-enhancement-suite" private;
+          "sponsorBlocker@ajay.app" = installExtention "sponsorblock" private;
+          "firefox@betterttv.net" = installExtention "betterttv" default;
         };
     };
 
