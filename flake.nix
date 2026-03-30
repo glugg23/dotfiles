11619cc8ef47618c357a8798574cc89ed43ada85
Max Leonhardt <m.leonhardt424@gmail.com>
2026-03-30 22:59:32 +0100

    fix nixpkgs flake inputs

--- flake.nix
+++ flake.nix
@@ -1,26 +1,28 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    nixpkgs-darwin.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin.url = "github:nix-darwin/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs-darwin";
 
     home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
-    nix-darwin.url = "github:nix-darwin/nix-darwin";
-    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs-unstable";
 
     nixos-hardware.url = "github:NixOS/nixos-hardware";
   };
   outputs =
     inputs@{
       self,
-      nixpkgs,
+      nixpkgs-unstable,
       home-manager,
+      nixpkgs-darwin,
       nix-darwin,
       nixos-hardware,
       ...
     }:
     {
-      nixosConfigurations.nixos-laptop = nixpkgs.lib.nixosSystem {
+      nixosConfigurations.nixos-laptop = nixpkgs-unstable.lib.nixosSystem {
         modules = [
           ./modules/hosts/nixos-laptop/configuration.nix
           home-manager.nixosModules.home-manager
@@ -35,7 +37,7 @@
           nixos-hardware.nixosModules.framework-amd-ai-300-series
         ];
       };
-      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.nixfmt-tree;
-      formatter.aarch64-darwin = nixpkgs.legacyPackages.aarch64-darwin.nixfmt-tree;
+      formatter.x86_64-linux = nixpkgs-unstable.legacyPackages.x86_64-linux.nixfmt-tree;
+      formatter.aarch64-darwin = nixpkgs-darwin.legacyPackages.aarch64-darwin.nixfmt-tree;
     };
 }
