79eb7830fa093e15b1c391f51f4128025661d9c9
Max Leonhardt <m.leonhardt424@gmail.com>
2026-04-13 22:45:20 +0100

    tidy up hm zsh

--- modules/zsh/home.nix
+++ modules/zsh/home.nix
@@ -11,7 +11,6 @@
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
     shellAliases = {
-      rebuild = "sudo nixos-rebuild switch --flake ~/dotfiles";
       update = "nix flake update --flake ~/dotfiles";
     };
     sessionVariables = {
