a1c0bf8a9a8e33a3fb62bb82c8b2911f08a3e487
Max Leonhardt <m.leonhardt424@gmail.com>
2026-05-02 11:47:32 +0100

    use new firefox config path
    TODO: update to home.stateVersion 26.05
    I've already had to migrate something for gtk4

--- modules/firefox.nix
+++ modules/firefox.nix
@@ -1,8 +1,9 @@
-{ ... }:
+{ config, ... }:
 
 {
   programs.firefox = {
     enable = true;
+    configPath = "${config.xdg.configHome}/mozilla/firefox";
     languagePacks = [ "en-GB" ];
 
     policies = {
