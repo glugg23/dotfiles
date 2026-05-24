29f1718a636b044d30410c9ddf0f22031c1eb735
Max Leonhardt <m.leonhardt424@gmail.com>
2026-05-24 23:45:56 +0100

    build grammar to ~/.local/share

--- modules/neovim.nix
+++ modules/neovim.nix
@@ -1,5 +1,33 @@
-{ config, ... }:
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 
+let
+  treeSitterGrammars = [
+    pkgs.tree-sitter.builtGrammars."tree-sitter-elixir"
+  ];
+
+  treeSitterParsers = pkgs.tree-sitter.withPlugins (_: treeSitterGrammars);
+
+  treeSitterQueries = pkgs.runCommandLocal "nvim-treesitter-queries" { } ''
+    mkdir -p "$out"
+
+    ${lib.concatMapStringsSep "\n" (
+      grammar:
+      let
+        lang = lib.replaceStrings [ "-" ] [ "_" ] (lib.removePrefix "tree-sitter-" (lib.getName grammar));
+      in
+      ''
+        if [ -d "${grammar}/queries" ]; then
+          ln -s "${grammar}/queries" "$out/${lang}"
+        fi
+      ''
+    ) treeSitterGrammars}
+  '';
+in
 {
   programs.neovim = {
     enable = true;
@@ -12,4 +40,7 @@
     source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/dotfiles/config/nvim";
     recursive = true;
   };
+
+  xdg.dataFile."nvim/site/parser".source = treeSitterParsers;
+  xdg.dataFile."nvim/site/queries".source = treeSitterQueries;
 }
