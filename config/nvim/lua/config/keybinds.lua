ce2ae027bd004345f0fd5c888285e3856262f9b8
Max Leonhardt <m.leonhardt424@gmail.com>
2026-05-30 22:49:52 +0100

    add neo-tree plugin

--- config/nvim/lua/config/keybinds.lua
+++ config/nvim/lua/config/keybinds.lua
@@ -10,3 +10,7 @@ vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find f
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
 vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+
+vim.keymap.set("n", "<leader>tt", "<Cmd>Neotree focus left<CR>")
+vim.keymap.set("n", "<leader>tr", "<Cmd>Neotree show reveal<CR>")
+vim.keymap.set("n", "<leader>tq", "<Cmd>Neotree close<CR>")
--- config/nvim/lua/config/plugins.lua
+++ config/nvim/lua/config/plugins.lua
@@ -2,10 +2,15 @@ vim.pack.add({
   -- dependencies
   "https://github.com/nvim-tree/nvim-web-devicons",
   "https://github.com/nvim-lua/plenary.nvim",
+  "https://github.com/MunifTanjim/nui.nvim",
   -- plugins
   "https://github.com/neanias/everforest-nvim",
   "https://github.com/nvim-lualine/lualine.nvim",
   "https://github.com/nvim-telescope/telescope.nvim",
+  {
+    src = "https://github.com/nvim-neo-tree/neo-tree.nvim",
+    version = vim.version.range("3")
+  },
   "https://github.com/tpope/vim-fugitive",
   "https://github.com/brenoprata10/nvim-highlight-colors",
 })
