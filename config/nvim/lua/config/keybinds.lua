3d6f292165cdf0947200380e2e3b374051022587
Max Leonhardt <m.leonhardt424@gmail.com>
2026-05-30 23:22:49 +0100

    add additional keybinds

--- config/nvim/lua/config/keybinds.lua
+++ config/nvim/lua/config/keybinds.lua
@@ -3,6 +3,11 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>cd", vim.cmd.Ex)
 vim.keymap.set("n", "<leader><leader>", vim.cmd.so)
 
+vim.keymap.set("n", "<leader>sw", ":%s/<C-r><C-w>/<C-r><C-w>/g<Left><Left>", { silent = false, desc = "Replace current word globally" })
+vim.keymap.set("n", "<leader>sg", ":%s///g<Left><Left><Left>", { silent = false, desc = "Replace any string globally" })
+
+vim.keymap.set("n", ":Q", ":qa")
+
 vim.keymap.set("n", "gl", vim.diagnostic.open_float)
 
 local builtin = require("telescope.builtin")
