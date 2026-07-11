f6eb2e5d66ecafe3e826a28afdcfdef6446442c6
Max Leonhardt <m.leonhardt424@gmail.com>
2026-07-12 00:01:09 +0100

    disable middle mouse paste

--- config/nvim/lua/config/keybinds.lua
+++ config/nvim/lua/config/keybinds.lua
@@ -6,8 +6,9 @@ vim.keymap.set("n", "<leader><leader>", vim.cmd.so)
 vim.keymap.set("n", "<leader>vt", "<Cmd>vert term<CR>")
 vim.keymap.set("n", "<leader>ht", "<Cmd>hor term<CR>")
 
-vim.keymap.set("n", "<leader>sw", ":%s/<C-r><C-w>/<C-r><C-w>/g<Left><Left>", { silent = false, desc = "Replace current word globally" })
+vim.keymap.set("n", "<leader>sw", ":s/<C-r><C-w>/<C-r><C-w>/g<Left><Left>", { silent = false, desc = "Replace current word" })
 vim.keymap.set("n", "<leader>sg", ":%s///g<Left><Left><Left>", { silent = false, desc = "Replace any string globally" })
+vim.keymap.set("n", "<leader>sgw", ":%s/<C-r><C-w>/<C-r><C-w>/g<Left><Left>", { silent = false, desc = "Replace current word globally" })
 
 vim.keymap.set("n", ":Q", ":qa")
 
@@ -22,3 +23,8 @@ vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help ta
 vim.keymap.set("n", "<leader>tt", "<Cmd>Neotree focus left<CR>")
 vim.keymap.set("n", "<leader>tr", "<Cmd>Neotree show reveal<CR>")
 vim.keymap.set("n", "<leader>tq", "<Cmd>Neotree close<CR>")
+
+vim.keymap.set("", "<MiddleMouse>", "<Nop>")
+for i = 2, 4 do
+  vim.keymap.set("", string.format("<%d-MiddleMouse>", i), "<Nop>")
+end
