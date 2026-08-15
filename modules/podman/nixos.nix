b083791ecfe51b49fb0ef6877e5940872ca0bf5d
Max Leonhardt <m.leonhardt424@gmail.com>
2026-08-15 18:10:22 +0100

    add qemu for podman

--- modules/podman/nixos.nix
+++ modules/podman/nixos.nix
@@ -1,9 +1,14 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   virtualisation.podman = {
     enable = true;
     dockerCompat = true;
     defaultNetwork.settings.dns_enabled = true;
+  };
+
+  boot.binfmt = {
+    emulatedSystems = [ "aarch64-linux" ];
+    preferStaticEmulators = true;
   };
 }
