23247ccb4b05d85c6c899ef3d9e84a8ef37e0e9e
Max Leonhardt <m.leonhardt424@gmail.com>
2026-09-06 16:30:42 +0100

    desktop base config before installing

---
+++ modules/hosts/nixos-desktop/configuration.nix
@@ -0,0 +1,61 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ./hardware-configuration.nix
+    ../../limine.nix
+    ../../nh.nix
+    ../../pipewire.nix
+    ../../zsh
+  ];
+
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  networking.hostName = "nixos-desktop";
+  networking.networkmanager.enable = true;
+
+  time.timeZone = "Europe/London";
+
+  i18n.defaultLocale = "en_GB.UTF-8";
+  console.keyMap = "uk";
+
+  # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
+  # services.xserver.enable = true;
+
+  services.displayManager.plasma-login-manager.enable = true;
+  services.desktopManager.plasma6.enable = true;
+
+  # Configure keymap in X11
+  # services.xserver.xkb = {
+  #   layout = "gb";
+  #   variant = "";
+  # };
+
+  users.users."max" = {
+    isNormalUser = true;
+    extraGroups = [
+      "max"
+      "networkmanager"
+      "wheel"
+    ];
+    shell = pkgs.zsh;
+  };
+
+  environment.systemPackages = with pkgs; [
+    fastfetch
+    git
+    tree
+  ];
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  nix.settings.experimental-features = [
+    "nix-command"
+    "flakes"
+  ];
+  nixpkgs.config.allowUnfree = true;
+
+  system.stateVersion = "26.11";
+}
