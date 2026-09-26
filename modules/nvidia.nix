e3f52505e37ff7ebf2cf096fd6edd8376b7dcdc5
Max Leonhardt <m.leonhardt424@gmail.com>
2026-09-26 11:19:51 +0100

    use non-tmpfs path for suspend video memory

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -3,6 +3,7 @@
     open = true;
     modesetting.enable = true;
     powerManagement.enable = true;
+    moduleParams.nvidia.NVreg_TemporaryFilePath = "/var/tmp";
   };
   services.xserver.videoDrivers = [ "nvidia" ];
 }
